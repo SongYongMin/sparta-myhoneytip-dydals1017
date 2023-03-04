3491ac1adbdf7faddf0b3b60ca171ccce2e2a11c
HappyUsSong <47716285+SongYongMin@users.noreply.github.com>
2023-03-04 14:42:48 +0900

    20230304_1442_3-4강 수강 후 상태바 적용 후

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -5,6 +5,7 @@ const main = 'https://storage.googleapis.com/sparta-image.appspot.com/lecture/ma
 import data from '../data.json';
 import Card from '../components/Card';
 import Loading from '../components/Loading';
+import { StatusBar } from 'expo-status-bar';
 export default function MainPage() {
   //useState 사용법
   //[state,setState] 에서 state는 이 컴포넌트에서 관리될 상태 데이터를 담고 있는 변수
@@ -13,7 +14,7 @@ export default function MainPage() {
   //모두 다 useState가 선물해줌
   //useState()안에 전달되는 값은 state 초기값
   const [state, setState] = useState([])
-  const [cateState,setCateState] = useState([])
+  const [cateState, setCateState] = useState([])
 
 
   //하단의 return 문이 실행되어 화면이 그려진다음 실행되는 useEffect 함수
@@ -54,6 +55,7 @@ export default function MainPage() {
       return 구문 안에서는 {슬래시 + * 방식으로 주석
     */
     <ScrollView style={styles.container}>
+      <StatusBar style="inverted" />
       <Text style={styles.title}>▷나만의 꿀팁!◁</Text>
       <Text style={styles.weather}>오늘의 날씨: {todayWeather + '°C ' + todayCondition} </Text>
       <Image style={styles.mainImage} source={{ uri: main }} />
@@ -115,13 +117,13 @@ const styles = StyleSheet.create({
     height: 60
   },
   middleButtonAll: {
-    width:100,
-    height:50,
-    padding:15,
-    backgroundColor:"#20b2aa",
-    borderColor:"deeppink",
-    borderRadius:15,
-    margin:7
+    width: 100,
+    height: 50,
+    padding: 15,
+    backgroundColor: "#20b2aa",
+    borderColor: "deeppink",
+    borderRadius: 15,
+    margin: 7
   },
   middleButton01: {
     width: 100,
@@ -163,16 +165,16 @@ const styles = StyleSheet.create({
     margin: 7
   },
   middleButtonText: {
-    color:"#fff",
-    fontWeight:"700",
+    color: "#fff",
+    fontWeight: "700",
     //텍스트의 현재 위치에서의 정렬 
-    textAlign:"center"
+    textAlign: "center"
   },
   middleButtonTextAll: {
-    color:"#fff",
-    fontWeight:"700",
+    color: "#fff",
+    fontWeight: "700",
     //텍스트의 현재 위치에서의 정렬 
-    textAlign:"center"
+    textAlign: "center"
   },
   cardContainer: {
     marginTop: 10,
